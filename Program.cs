2fad25a639f50f5f3b9f1a936731f958eec7503a
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-06-12 09:55:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a word");
+            string word = Console.ReadLine();
+            word = word.ToLower();
+            string newline = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 1; j < word.Length; j++)
+                {
+                    if (word[i] == word[j])
+                    {
+                        newline = newline.Insert(i,")");
+                        break;
+                        
+                    }
+                    else
+                    {
+                        newline = newline.Insert(i, "(");
+                        break;
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine(newline);
+            
+
+
+
+        }
+
+        
+        
+        
+       
+    }
+}
